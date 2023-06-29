25e1a01c4ab9ff5ba389b7747df2bdfce4cc442a
TechSammy <mrsam03102002@gmail.com>
2023-06-29 23:04:08 +0530

    Working with Coin search component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {ThemeProvider} from './context/ThemeConetext'
 import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './routes/Home';
+import Signin from './routes/Signin';
+import Signup from './routes/Signup';
+import Account from './routes/Account';
+import axios from 'axios';
+
 
 function App() {
+
+  const [coins, setCoins] = useState([])
+
   return <ThemeProvider>
     <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/signin' element={<Signin/>} />
+      <Route path='/signup' element={<Signup/>} />
+      <Route path='/account' element={<Account/>} />
+    </Routes>
   </ThemeProvider>
 }
 
